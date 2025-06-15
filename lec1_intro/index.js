5cd4a6d5dd4a80070743062b4fae741af9445510
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-15 16:56:24 +0600

    using nodemon

--- lec1_intro/index.js
+++ lec1_intro/index.js
@@ -2,3 +2,8 @@ console.log("first line");
 console.log("second line");
 console.log(2 + 3);
 console.log("third line");
+let firstName = "John";
+console.log("fourth line");
+console.log("fifth line");
+console.log(firstName);
+console.log("sixth line");
