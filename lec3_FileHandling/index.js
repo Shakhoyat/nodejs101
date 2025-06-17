d5c09ea1e76808d9dd1a5eaaf6de185bfa383645
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-17 12:36:33 +0600

    feat: add append file functionality and update sample text content

--- lec3_FileHandling/index.js
+++ lec3_FileHandling/index.js
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile, appendFile } from "fs/promises";
 
 //Read a file
 const readFileAsync = async (filename) => {
@@ -22,4 +22,17 @@ const writeFileAsync = async (filename, content) => {
   }
 };
 
-writeFileAsync("sample.txt", "Hello, this is a test content!");
+// writeFileAsync("sample.txt", "Hello, this is a test content!");
+
+//append to a file
+const appendFileAsync = async (filename, content) => {
+  try {
+    await appendFile(filename, content, "utf-8");
+    console.log("Content appended successfully");
+  } catch (error) {
+    console.error("Error appending to file:", error);
+  }
+};
+
+appendFileAsync("sample.txt", "\nThis is appended content!");
+readFileAsync("sample.txt");
