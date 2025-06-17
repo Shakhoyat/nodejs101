a00a42b3980332275ef9d5632efedd8d50fca4f2
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-17 16:30:57 +0600

    Add iPhone list rendering to EJS template

--- lec11_template_engine/server.js
+++ lec11_template_engine/server.js
@@ -1,9 +1,27 @@
 import express from "express";
 
 const app = express();
+let iphones = [
+  { title: "iPhone 14", price: 999, image: "https://example.com/iphone14.jpg" },
+  {
+    title: "iPhone 14 Pro",
+    price: 1099,
+    image: "https://example.com/iphone14pro.jpg",
+  },
+  {
+    title: "iPhone 14 Pro Max",
+    price: 1199,
+    image: "https://example.com/iphone14promax.jpg",
+  },
+  {
+    title: "iPhone SE",
+    price: 429,
+    image: "https://example.com/iphonese.jpg",
+  },
+];
 app.get("/", (req, res) => {
   let name = "John Doe";
-  res.render("index.ejs", { name });
+  res.render("index.ejs", { name, iphones });
 });
 
 const PORT = 3000;
