e0e8fc42c2297aa797c9270ac92ee1d3a3fb3dc8
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-17 12:59:21 +0600

    feat: uncomment console logs for absolute path and file extension checks

--- lec4_pathModule/index.js
+++ lec4_pathModule/index.js
@@ -9,5 +9,14 @@ const joinedPath = path.join("/path", "index.py", "file.java");
 
 //absolute path
 const absolutePath = path.resolve();
-console.log("We are currently working on =======", absolutePath);
+// console.log("We are currently working on =======", absolutePath);
 // console.log("Current Directory========", process.cwd());
+
+//extension of a file
+const fileExtension = path.extname("resume.pdf");
+// console.log("File Extension=========", fileExtension);
+if (fileExtension === ".pdf") {
+  console.log("This is a PDF file");
+} else {
+  console.log("This is not supported file");
+}
