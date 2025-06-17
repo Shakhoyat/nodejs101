59ec4fce1be445653edd6d26786d8d534cec28f4
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-17 14:43:38 +0600

    express installation

---
+++ lec7_Express/server.js
@@ -0,0 +1,5 @@
+console.log("Starting Express Server");
+console.log(
+  "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for building web and mobile applications."
+);
+import express from "express";
