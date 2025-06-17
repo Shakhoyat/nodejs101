e584567975ecdded8d8ff29bd3b4bf9ab580610f
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-17 15:17:02 +0600

    routing in express

--- lec8_Express_server/server.js
+++ lec8_Express_server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 
 const app = express();
-const PORT = 2000;
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("you are requested the root URL");
+});
+
+app.get("/srs", (req, res) => {
+  res.send("Welcome to Statistical Response Suite (SRS)");
+});
 app.listen(PORT, () => {
   console.log(`Express server is running on port ${PORT}`);
 });
